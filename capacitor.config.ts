cde9dc5c93e9fa88d32baa97a7b0709c5c3309c2
allin1appd-sys <allin1.appd@gmail.com>
2026-03-13 00:59:50 +0200

    Remove remote server URL for production mobile build

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,10 +4,6 @@ const config: CapacitorConfig = {
   appId: "com.homehub.app",
   appName: "HomeHub",
   webDir: "dist",
-  server: {
-    url: "https://2fc7b9e2-e7d0-41b3-9b05-178a47f441b8.lovableproject.com?forceHideBadge=true",
-    cleartext: true,
-  },
 };
 
 export default config;
