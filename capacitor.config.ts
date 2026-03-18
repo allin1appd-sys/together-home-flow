2b65a45c2a4354147be67d5bcb218b723a92c628
allin1appd-sys <allin1.appd@gmail.com>
2026-03-18 02:02:16 +0200

    Update appId to match Apple bundle ID

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: "com.homehub.app",
+  appId: "com.homehub.appname",
   appName: "HomeHub",
   webDir: "dist",
 };
